9d66d8cc5b130afc451b9c14bb7d28cc7dfbd5ee
Khoa <Khoapnse184368@fpt.edu.vn>
2025-10-23 07:47:14 +0700

    update chatbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // ====== RAG (BM25) + LLM (Worker API) ======
-const WORKER_URL = "https://api.mln-chatbot.online/";
+const WORKER_URL = "https://api.mln-chatbot.online";
 let chatAI = null;  // RAG object
 
 // Prompt cứng
